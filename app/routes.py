6649fa9e171fe20e9b900770e0bb18a66645133b
Mike Jones <177jones@pm.me>
2020-11-25 01:05:15 -0800

    fix

--- app/routes.py
+++ app/routes.py
@@ -27,7 +27,7 @@ def showredacted():
     data = g.db.execute('select * from cards').fetchall()
     print(data)
     g.db.close()
-    message = jsonify(data)
+    message = json.dumps(data)
     print(message)
     return render_template('message.html', message=message)
 
