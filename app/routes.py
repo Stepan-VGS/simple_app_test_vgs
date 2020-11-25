4c65c4ac907ba73fd8d4b6532bcbbad3ef6e559f
Mike Jones <177jones@pm.me>
2020-11-24 16:08:57 -0800

    pem update

--- app/routes.py
+++ app/routes.py
@@ -22,7 +22,7 @@ def forward():
     os.environ['HTTPS_PROXY'] = 'https://UStuxaJU5RVKd7JC4GWWZN1f:93390e04-3643-4f21-b277-c1bc0852de60@tntnopmrps6.SANDBOX.verygoodproxy.com:8080'
     res = requests.post('https://echo.apps.verygood.systems/post',
                         json={'message': message},
-                        verify='/sandbox.pem')
+                        verify='sandbox.pem')
 
     res = res.json()
     return render_template('forward.html', response=res)
