9babbcfd07eff96df2491ce09838f20c69f93d0c
Justin Casey <jcaseydev@gmail.com>
2021-11-28 15:18:12 -0800

    Completed VGS test task

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # simple_app_test_vgs
+
+This is a simple program to demonstrate aliasing and unaliasing of credit card data. It works by taking in your credit card information (number, expiration date, cvc) and aliases that information for better security. You can then, use those aliases to get the orignal data back.
+
+## Set up
+Create a python virtual environment and activate it
+```
+python3 -m venv env
+source env/bin/activate
+```
+Install the needed requirements with:
+```
+pip install -r requirements.txt
+```
+
+#### Create .env file
+Create a .env file at the root of the project and populate with the fields below:
+```
+PROXY_USERNAME=
+PROXY_PASSWORD=
+VAULT_ID=
+PORT=8080
+```
+
+The PROXY_USERNAME and PROXY_PASSWORD can be found by going to VGS Dashboard -> Vault Settings -> Access Credentials
+The VAULT_ID can be found at the top left of the VGS Dashboard
+#### Run the app
+In a terminal run the following command to start the application
+```
+python3 simple_app.py
+```
+
+### Usage instructions
+1. navigate to 127.0.0.1:5000
+2. Input your name, card number, card expiration date, and card cvc
+3. Click "Redact" to see the aliased data
+4. Click "Reveal" again to see the original data
--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,69 @@
 from app import app
-from flask import render_template, request, jsonify
+from flask import render_template, request
+from dotenv import load_dotenv
 import requests
 import os
+import json
 
+load_dotenv()
+# get config vars from env file
+USERNAME = os.getenv('PROXY_USERNAME')
+PASSWORD = os.getenv('PROXY_PASSWORD')
+VAULT_ID = os.getenv('VAULT_ID')
+PORT = os.getenv('PORT')
 
 @app.route('/', methods=['GET'])
 def index():
+    '''
+    render the index page
+
+    Index pages has fields for user to
+    enter credit card info (name, number, expiration, cvc)
+    '''
     return render_template('index.html')
 
 
-@app.route('/add_message', methods=['POST'])
-def add_message():
-    message = request.form['message']
-    return render_template('message.html', message=message)
+@app.route('/redact', methods=['POST'])
+def redact():
+    '''
+    Take the data from index page and send to 
+    VGS to alias the card number, expiration and CVC
+    data. Card holder will not be aliased.
 
+    After recieving the aliased data, render the message.html 
+    page and show the aliased data.
+    '''
+    card_holder = request.form['card_holder']
+    original_card_num = request.form['card_num']
+    original_card_exp = request.form['card_exp']
+    original_card_cvc = request.form['card_cvc']
 
-@app.route("/forward", methods=['POST'])
-def forward():
-    message = request.form['message']
-
-    os.environ['HTTPS_PROXY'] = 'https://USERNAME:PASSWORD@{your_tenant_id}.SANDBOX.verygoodproxy.com:8080'
-    res = requests.post('https://echo.apps.verygood.systems/post',
-                        json={'message': message},
-                        verify='{path_to_cert_file}')
+    res = requests.post(f"https://{VAULT_ID}.sandbox.verygoodproxy.com/post",
+                        json={'card_holder': card_holder, 'card_number': original_card_num, 'card_cvc': original_card_cvc, 'card_exp': original_card_exp}, 
+                        verify=False)
 
     res = res.json()
-    return render_template('forward.html', response=res)
+    data = json.loads(res['data'])
+
+    return render_template('message.html', data=data)
+
+
+@app.route("/reveal", methods=['POST'])
+def reveal():
+    '''
+    Take the data from the message.html page and reveal
+    the original data.
+    '''
+    aliased_card_num = request.form['card_num']
+    aliased_card_exp = request.form['card_exp']
+    aliased_card_cvc = request.form['card_cvc']
+    
+    os.environ['HTTPS_PROXY'] = f'https://{USERNAME}:{PASSWORD}@{VAULT_ID}.sandbox.verygoodproxy.com:{PORT}'
+
+    res = requests.post('https://echo.apps.verygood.systems/post',
+                        json={'card_number': aliased_card_num, 'card_exp': aliased_card_exp, 'card_cvc': aliased_card_cvc},
+                        verify='app/secrets/cert.pem')
+
+    data = json.loads(res.json()['data'])
+
+    return render_template('forward.html', data=data)
